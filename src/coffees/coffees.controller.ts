8dfcb2551a7f42c63932feb86db0fc627573929c
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-08-03 11:18:08 +0530

    crud success

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller,Delete,Get, Param, Patch, Post, Res } from '@nestjs/common';
+
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+
+    @Get()
+    findAll(@Res() response){
+        response.status(200).send("get method success")
+    }
+    @Get(':id')
+    findone(@Param('id') id:number){
+        return `This action returns ${id} in signle value`
+    }
+    @Post()
+    create(@Body() body){
+        return body
+    }
+    @Patch(':id')
+    update(@Param('id') id:number,@Body() body){
+        return `patch updated ${id} is success`
+    }
+    @Delete(':id')
+    delete(@Param('id') id:number,@Res() response){
+        response.status(200).send(`data will be deleted on ${id}`)
+    }
+}
+ 
